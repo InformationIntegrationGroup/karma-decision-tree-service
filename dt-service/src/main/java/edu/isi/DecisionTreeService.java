8b01c9fbb1d41070cf3684fe1361b01eb087a6de
Ayush Jaiswal <mail.ayush.jaiswal@gmail.com>
2014-03-21 03:40:27 +0530

    Modified R scripts and added methods to handle training and testing requests.

--- dt-service/src/main/java/edu/isi/DecisionTreeService.java
+++ dt-service/src/main/java/edu/isi/DecisionTreeService.java
@@ -17,8 +17,89 @@ import javax.ws.rs.core.Response;
 @Path("/dt")
 public class DecisionTreeService {
     
+    private final String newline = System.getProperty("line.separator");
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy_HmsS");
+    private String resourcesPath = (new File((getClass().getResource("/")).getFile())).getAbsolutePath() + "/R_Scripts";
+
+    
+    /** Saves URL Encoded type String as a CSV file
+     * 
+     * @param data URL Encoded data
+     * @param filepath Path of the file where the data has to be saved as CSV
+     * 
+     */
+    private void saveCSV(String data, String filePath) throws IOException {
+
+        FileWriter writer = new FileWriter(filePath);
+        String[] rows = data.split(newline);
+        for(String rowString : rows) {
+
+                String[] row = rowString.split("\t");
+                for(int i = 0; i < row.length; i++)
+                {
+                    writer.append(row[i]);
+                    if(i != row.length - 1)
+                    writer.append(",");
+                }
+                writer.append(newline);
+            }
+            writer.close();
+    
+    }
+
+    /** Executes the given command and returns the response as a String
+     * 
+     * @param command The command to be executed.
+     * 
+     * @return Response of the execution of the command.
+     */
+    private String executeCommand(String command) throws Exception {
+
+        Process pr = Runtime.getRuntime().exec(command);
+        BufferedReader prInput = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+        String line = null;
+        String response = "";
+        while((line = prInput.readLine()) != null) {
+        
+               response += line;
+               response += newline;
+        
+        }
+        return response;
+
+    }
+
+    /** Deletes a file
+     * 
+     * @param filePath Path of the file to be deleted.
+     * 
+     */
+    private void delete(String filePath) throws Exception {
+
+        File f = new File(filePath);
+        f.delete();
+
+    }
+
+    /** Returns path of R model given it's name. The model can be saved/accessed at this path.
+     * Also makes sure that the 'models' directory exists
+     * 
+     * @param modelName Unique name of the model
+     * 
+     * @return Path at which the model can be saved/accessed.
+     */
+    private String getModelPath(String modelName) {
+
+        String modelDirectoryPath = resourcesPath + "/models";
+        File dir = new File(modelDirectoryPath);
+        dir.mkdir();
+        return modelDirectoryPath + "/" + modelName + ".RData";        
+
+    }
+
     /** Method processing HTTP GET requests, producing "text/plain" MIME media
      * type.
+     * 
      * @return String that will be send back as a response of type "text/plain".
      */
     @GET 
@@ -26,4 +107,63 @@ public class DecisionTreeService {
     public String getIt(){
         return "Works!";
     }
+
+    /** Method handling request for training a Decision Tree model with training data
+     * sent as POST, produces "text/plain" summary of the trained model.
+     * 
+     * @param data Training data
+     * 
+     * @return String that contains details of trained model and unique model-name.
+     */
+    @POST
+    @Path("/train")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response train(String data) throws Exception {
+
+        String dataPath = resourcesPath + "/Train_" + sdf.format(Calendar.getInstance().getTime()) + ".csv";
+        saveCSV(data, dataPath);
+
+
+        String modelName = "DT_" + sdf.format(Calendar.getInstance().getTime()) + "_model";
+        String modelPath = getModelPath(modelName);
+        
+        String command = "Rscript " + resourcesPath + "/DTTraining.R " + dataPath + " " + modelName;
+        String response = executeCommand(command);
+        response += newline + modelName + " created!" + newline;
+        
+        delete(dataPath);
+
+        return Response.status(200).entity(response).build();
+    }
+
+    /** Method handling request for testing a Decision Tree model with testing data
+     * sent as POST, produces "text/plain" confusion matrix or predicted labels, depending on
+     * specified output-type
+     * 
+     * @param data Testing data
+     * @param outputType takes value: "predictions" or "confusion_matrix" to decide output-type for testing
+     * 
+     * @return Output of the testing phase in the outputType format
+     */
+    @POST
+    @Path("/test")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response test(String data, @QueryParam("modelName") String modelName,
+                        @QueryParam("outputType") String outputType) throws Exception {
+
+        String dataPath = resourcesPath + "/Test_" + sdf.format(Calendar.getInstance().getTime()) + ".csv";
+        saveCSV(data, dataPath);
+
+        String modelPath = getModelPath(modelName);
+        
+        String command = "Rscript " + resourcesPath + "/DTTesting.R " + dataPath + " " + modelPath + " " + outputType;
+        String response = executeCommand(command);
+
+        delete(dataPath);
+
+        return Response.status(200).entity(response).build();
+    }
+
 }
--- dt-service/src/main/resources/R_Scripts/DTTraining.r
+++ dt-service/src/main/resources/R_Scripts/DTTraining.r
@@ -2,12 +2,11 @@ library(rpart)
 
 args <- commandArgs(TRUE)
 dataset <- read.csv(args[1])
-model <- args[2]
+modelFilePath <- args[2]
 cols <- names(dataset)
 n_cols <- length(cols)
 # Assuming that the last column has the labels
-fmla <- as.formula(paste(paste(cols[n_cols], " ~ "), paste(cols[1:n_cols-1], collapse = "+")))
+fmla <- as.formula(paste(cols[n_cols], "~", paste(cols[1:n_cols-1], collapse = "+")))
 DTModel <- rpart(fmla, data = dataset, method = "class")
-filename <- paste(model, '.RData', sep = "")
-save(DTModel, file = filename)
+save(DTModel, file = modelFilePath)
 summary(DTModel)
